9a91594635507b2af2280353bc1b9c81b3e78b6b
Barbara-D <bdeskar@gmail.com>
2021-07-09 23:35:53 +0200

    create main layout and sidenav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
+	declarations: [AppComponent, MainLayoutComponent, SidenavComponent],
+	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatSidenavModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
